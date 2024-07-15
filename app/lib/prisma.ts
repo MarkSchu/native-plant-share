2de66abf54ba087600393330f2cc0b541b275525
Mark Schumaker <markschumaker@adsl-172-10-0-160.dsl.sndg02.sbcglobal.net>
2024-07-15 08:47:51 -0400

    install prisma, get basics set up

---
+++ app/lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
+}
+
+// import prisma into file where/when you want to do DB work!
+export default prisma;
