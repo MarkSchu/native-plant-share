ed5d8181977749264d8530b6eb87878033205a0e
Mark Schumaker <markschumaker@marks-mbp-2.mynetworksettings.com>
2024-07-21 08:36:17 -0400

    shared plant form successfully passes data to action

--- app/lib/actions/index.tsx
+++ app/lib/actions/index.tsx
@@ -5,28 +5,27 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
-const CreateInvoice = z.object({
+const CreateSharedPlant = z.object({
   commonName: z.string(),
   latinName: z.string(),
   description: z.string(),
+  growthForm: z.string(),
+  height: z.string(),
+  sun: z.string(),
+  soil: z.string(),
 });
 
+
+
 export async function createSharedPlant(formData: FormData) {
-   
-  const { commonName, latinName, description } = CreateInvoice.parse({
+  const sharedPlant = CreateSharedPlant.parse({
     commonName: formData.get('commonName'),
     latinName: formData.get('latinName'),
-    description: formData.get('description')
+    description: formData.get('description'),
+    growthForm: formData.get('growthForm'),
+    height: formData.get('height'),
+    sun: formData.getAll('sun').join(','),
+    soil: formData.getAll('soil').join(',')
   });
-
-  const sharedPlant = await prisma.sharedPlant.create({
-    data: {
-      commonName,
-      latinName,
-      description
-    }
-  });
-
-  console.log(sharedPlant);
 }
 
--- app/share/components/new-plant-form/index.tsx
+++ app/share/components/new-plant-form/index.tsx
@@ -8,6 +8,17 @@ interface InputProps {
   required: boolean;
 }
 
+interface CheckboxGroupOption {
+  name: string;
+  label: string;
+}
+
+interface CheckboxGroupProps {
+  label: string;
+  name: string;
+  options: CheckboxGroupOption[];
+}
+
 function TextArea({name, label, required} : InputProps) {
   return (
     <div>
@@ -35,26 +46,103 @@ function Input({type, name, label, required} : InputProps) {
   )
 }
 
+function CheckboxGroup({ label, name, options }: CheckboxGroupProps) {
+  return (
+    <fieldset>
+      <label>{label}</label>
+      {options.map((option: CheckboxGroupOption) => 
+        <div key={option.name}>
+          <input 
+            type="checkbox"
+            value={option.name}
+            name={name}
+          />
+          <label>{option.label}</label>
+        </div>
+      )}
+    </fieldset>
+  )
+}
+
 export function NewPlantForm() {
   return (
     <form action={createSharedPlant}>
+
+      {/* Common Name */}
       <Input 
         type="text" 
         name="commonName" 
         label="Common Name"
         required={true}
       />
+
+      {/* Latin Name */}
       <Input 
         type="text" 
         name="latinName" 
         label="Latin Name"
         required={true}
       />
+
+      {/* Description */}
       <TextArea 
         name="description" 
         label="Describe What You've Got"
         required={true}
       />
+
+      {/* Height */}
+      <Input 
+        type="text" 
+        name="height" 
+        label="Height"
+        required={true}
+      />
+      
+      {/* Type/Form */}
+      <select 
+        name="growthForm"
+      >
+        {([
+          'annual',
+          'daylily',
+          'ferns',
+          'grasses',
+          'herb',
+          'perennial',
+          'vine',
+          'tree'
+        ]).map((growthForm, i) =>
+          <option 
+            key={growthForm} 
+            value={growthForm}>
+              {growthForm}
+            </option>
+        )}
+      </select>
+
+      {/* Sun */}
+      <CheckboxGroup 
+        label="Sun"
+        name="sun"
+        options={[
+          {label: 'Full Sun', name: 'fullsun'},
+          {label: 'Partial Shade', name: 'partshade'},
+          {label: 'Shade', name: 'shade'},
+        ]}
+      />
+
+      {/* Soil */}
+      <CheckboxGroup
+        label="Soil"
+        name="soil"
+        options={[
+          {label: 'Dry', name: 'dry'},
+          {label: 'Moist', name: 'moist'},
+          {label: 'West', name: 'wet'},
+        ]}
+      />
+
       <input 
         type="submit" 
         value="Create!" 
